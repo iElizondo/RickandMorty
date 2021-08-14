6bb18acd04e17b0c7d6a82314cf7e7cf1846dc80
iElizondo <isabel.elizondo.meza@est.una.ac.cr>
2021-08-13 20:40:23 -0600

    Characters components

--- src/app/components/pages/characters/characters-card/characters-card.component.ts
+++ src/app/components/pages/characters/characters-card/characters-card.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Characters } from '@app/shared/interdfaces/data.interfaces';
 
 @Component({
   selector: 'app-characters-card',
   templateUrl: './characters-card.component.html',
-  styleUrls: ['./characters-card.component.scss']
+  styleUrls: ['./characters-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CharactersCardComponent implements OnInit {
+export class CharactersCardComponent {
+  @Input() character: Characters;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  getIcon(): string {
+    return this.character.isFavorite ? 'heart-solid.svg' : 'heart.svg';
   }
-
+  toggleFavorite(): void {
+    const isFavorire = this.character.isFavorite;
+    this.getIcon();
+    this.character.isFavorite = !isFavorire;
+  }
 }
